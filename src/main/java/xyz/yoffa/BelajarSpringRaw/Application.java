4aec44e016c0b76625b2df24ab30c8ff0853c6f0
Sayyid Yofa <sayyidyofa@gmail.com>
2021-07-26 16:07:18 +0700

    Day 4: Literal values injection via XML config

--- src/main/java/xyz/yoffa/BelajarSpringRaw/Application.java
+++ src/main/java/xyz/yoffa/BelajarSpringRaw/Application.java
@@ -2,6 +2,7 @@ package xyz.yoffa.BelajarSpringRaw;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import xyz.yoffa.BelajarSpringRaw.coach.Coach;
+import xyz.yoffa.BelajarSpringRaw.coach.CricketCoach;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,12 +11,18 @@ public class Application {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // retrieve the bean from container
-        Coach leCoach = context.getBean("belalang", Coach.class);
+        CricketCoach leCoach = context.getBean("belalang", CricketCoach.class);
 
         // call methods on the bean
         System.out.println(leCoach.getDailyWorkout());
 
         System.out.println(leCoach.getDailyFortune());
+
+        // get literal email
+        System.out.println(leCoach.getEmailAddress());
+
+        // get literal team
+        System.out.println(leCoach.getTeam());
 
         // close the context
         context.close();
--- src/main/java/xyz/yoffa/BelajarSpringRaw/coach/CricketCoach.java
+++ src/main/java/xyz/yoffa/BelajarSpringRaw/coach/CricketCoach.java
@@ -6,8 +6,30 @@ public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    private String emailAddress;
+
+    private String team;
+
     public CricketCoach() {
         System.out.println("Cricket coach");
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Setting email address...");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Setting team...");
+        this.team = team;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
     public void setFortuneService(FortuneService leFortuneService) {
