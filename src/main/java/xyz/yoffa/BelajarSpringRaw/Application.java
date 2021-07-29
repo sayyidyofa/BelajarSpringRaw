5c7b31c095948350212aa227fef9d7c07bf5dd7e
Sayyid Y <sayyidyofa@gmail.com>
2021-07-29 12:04:07 +0700

    Day 5: Bean scopes, in XML

--- src/main/java/xyz/yoffa/BelajarSpringRaw/Application.java
+++ src/main/java/xyz/yoffa/BelajarSpringRaw/Application.java
@@ -3,26 +3,25 @@ package xyz.yoffa.BelajarSpringRaw;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import xyz.yoffa.BelajarSpringRaw.coach.Coach;
 import xyz.yoffa.BelajarSpringRaw.coach.CricketCoach;
+import xyz.yoffa.BelajarSpringRaw.coach.SoccerCoach;
 
 public class Application {
     public static void main(String[] args) {
 
         // load the app context
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScopeContext.xml");
 
         // retrieve the bean from container
-        CricketCoach leCoach = context.getBean("belalang", CricketCoach.class);
 
-        // call methods on the bean
-        System.out.println(leCoach.getDailyWorkout());
+        Coach coach1 = context.getBean("myCoach", Coach.class);
 
-        System.out.println(leCoach.getDailyFortune());
+        Coach coach2 = context.getBean("myCoach", Coach.class);
 
-        // get literal email
-        System.out.println(leCoach.getEmailAddress());
+        System.out.println("Coach 1 memory location: " + coach1);
 
-        // get literal team
-        System.out.println(leCoach.getTeam());
+        System.out.println("Couch 2 memory location: " + coach2);
+
+        System.out.println("Do coach 1 and coach 2 point to the same object? " + (coach1 == coach2 ? "Yes" : "No"));
 
         // close the context
         context.close();
