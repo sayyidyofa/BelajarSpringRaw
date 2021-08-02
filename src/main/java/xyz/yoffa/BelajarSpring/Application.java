068bb520b6cf10c52374e93373a486cbb3a2c812
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-02 12:41:01 +0700

    Day 8: Trying AnnotationConfigApplicationContext

--- src/main/java/xyz/yoffa/BelajarSpring/Application.java
+++ src/main/java/xyz/yoffa/BelajarSpring/Application.java
@@ -1,5 +1,19 @@
 package xyz.yoffa.BelajarSpring;
 
-public class Application {
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import xyz.yoffa.BelajarSpringRaw.teh.Teh;
 
+public class Application {
+    public static void main(String[] args) {
+
+        // Fire up a Spring IoC Container via Java class config
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+        Teh leTeh = context.getBean("tehCelup", Teh.class);
+
+        System.out.println("Cara Penyajian leTeh: " + leTeh.penyajian());
+
+        // close the context
+        context.close();
+    }
 }
