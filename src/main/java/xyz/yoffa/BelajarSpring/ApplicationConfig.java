eeed4c5b6a9b36ef4666b931b7ac3a687c9cfcf2
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-02 12:40:22 +0700

    Day 8: (for real now) Goodbye XML config

---
+++ src/main/java/xyz/yoffa/BelajarSpring/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package xyz.yoffa.BelajarSpring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("xyz.yoffa")
+public class ApplicationConfig {
+}
