febc8e6f76cfb575d7b7aad4fc25c0f55f8539f2
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-02 10:04:17 +0700

    Day 8: Bean Lifecycle methods, [at]PostConstruct and [at]PreDestroy

--- src/main/java/xyz/yoffa/BelajarSpringRaw/teh/TehSeduh.java
+++ src/main/java/xyz/yoffa/BelajarSpringRaw/teh/TehSeduh.java
@@ -1,5 +1,6 @@
 package xyz.yoffa.BelajarSpringRaw.teh;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +9,12 @@ import org.springframework.stereotype.Component;
 import xyz.yoffa.BelajarSpringRaw.fortune.FortuneService;
 import xyz.yoffa.BelajarSpringRaw.sweetener.Sweetener;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope("prototype")
-public class TehSeduh implements Teh{
+public class TehSeduh implements Teh {
 
     private Sweetener sweetener;
 
@@ -20,6 +24,11 @@ public class TehSeduh implements Teh{
     // @Autowire is not required for constructors since Spring 4.3
     public TehSeduh(@Qualifier("sugarCane") Sweetener sweetener) {
         this.sweetener = sweetener;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println(this + " has been constructed");
     }
 
     @Override
@@ -32,4 +41,8 @@ public class TehSeduh implements Teh{
         return sweetener.getCalories();
     }
 
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("Going to destroy " + this);
+    }
 }
