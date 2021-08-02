a622520fa09dda9295b311d94ee678037e514e7e
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-02 12:41:17 +0700

    Day 8: Configure logging

---
+++ src/main/java/xyz/yoffa/BelajarSpring/util/LogConfig.java
@@ -0,0 +1,46 @@
+package xyz.yoffa.BelajarSpring.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+@Component
+@PropertySource("classpath:belajarspring.properties")
+public class LogConfig {
+    @Value("${root.logger.level}")
+    private String rootLoggerLevel;
+
+    @Value("${printed.logger.level}")
+    private String printedLoggerLevel;
+
+    @PostConstruct
+    public void initLogger() {
+        // parse levels
+        Level rootLevel = Level.parse(rootLoggerLevel);
+        Level printedLevel = Level.parse(printedLoggerLevel);
+
+        // get logger for app context
+        Logger applicationContextLogger = Logger.getLogger(AnnotationConfigApplicationContext.class.getName());
+
+        // get parent logger
+        Logger loggerParent = applicationContextLogger.getParent();
+
+        // set root logging level
+        loggerParent.setLevel(rootLevel);
+
+        // set up console handler
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(printedLevel);
+        consoleHandler.setFormatter(new SimpleFormatter());
+
+        // add handler to the logger
+        loggerParent.addHandler(consoleHandler);
+    }
+}
