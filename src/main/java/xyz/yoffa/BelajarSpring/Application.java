ffc7da943ebcca96161273b91e707d3d80590ec8
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-04 07:05:15 +0700

    Day 9: [at]Bean for manually create beans in methods

--- src/main/java/xyz/yoffa/BelajarSpring/Application.java
+++ src/main/java/xyz/yoffa/BelajarSpring/Application.java
@@ -1,7 +1,7 @@
 package xyz.yoffa.BelajarSpring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import xyz.yoffa.BelajarSpringRaw.teh.Teh;
+import xyz.yoffa.BelajarSpring.serving.Serving;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,9 +9,9 @@ public class Application {
         // Fire up a Spring IoC Container via Java class config
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-        Teh leTeh = context.getBean("tehCelup", Teh.class);
+        Serving pesanan = context.getBean("paketMiskin", Serving.class);
 
-        System.out.println("Cara Penyajian leTeh: " + leTeh.penyajian());
+        System.out.println("Minuman di pesanan: " + pesanan.getDrink());
 
         // close the context
         context.close();
--- src/main/java/xyz/yoffa/BelajarSpring/ApplicationConfig.java
+++ src/main/java/xyz/yoffa/BelajarSpring/ApplicationConfig.java
@@ -1,9 +1,22 @@
 package xyz.yoffa.BelajarSpring;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import xyz.yoffa.BelajarSpring.beverage.Beverage;
+import xyz.yoffa.BelajarSpring.beverage.PlainWater;
+import xyz.yoffa.BelajarSpring.serving.PaketMiskin;
+import xyz.yoffa.BelajarSpring.serving.Serving;
 
 @Configuration
-@ComponentScan("xyz.yoffa")
 public class ApplicationConfig {
+
+    @Bean
+    public Beverage plainWater() {
+        return new PlainWater();
+    }
+
+    @Bean
+    public Serving paketMiskin() {
+        return new PaketMiskin(plainWater());
+    }
 }
